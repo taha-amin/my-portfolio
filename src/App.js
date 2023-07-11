fcad5aadb7a0a5162e8857a985fcfcee16aeb782
Mohamed Amin <mohamedamin2009ster@gmail.com>
2023-07-11 14:44:41 -0700

    created About component and added to app.js. Edited text in p tag for home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
+import About from "./Components/About";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <About />
     </div>
   );
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -14,14 +14,9 @@ const Home = () => {
           I'm a Software Engineer
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
-          Passionate about software engineering and full-stack development, I am
-          a results-driven professional with experience in building high-quality
-          mobile and web applications. With a strong foundation in JavaScript,
-          React, and Node.js, I thrive in collaborative environments, leveraging
-          my skills to create innovative solutions. I am motivated by challenges
-          and dedicated to delivering exceptional user experiences. Let's
-          connect and explore opportunities to contribute to exciting projects
-          together!
+          Full Stack Software Engineer with a fondness for clean, quality tested
+          code and a proponent for communication and problem solving. Quickly
+          adaptable, humble pupil, and proven self-learner.
         </p>
         <div>
           <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
