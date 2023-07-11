99ff8f9a3c1070f04de262805b32c7223e4c38fb
Mohamed Amin <mohamedamin2009ster@gmail.com>
2023-07-10 17:59:47 -0700

    added state to navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import ProfilePicture from "../Assets/profilepicture.png";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
   return (
     <div className="fixed w-full h-[80px] flex justified-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
@@ -20,18 +24,24 @@ const Navbar = () => {
       </ul>
 
       {/* Hamburger */}
-      <div className="md:hidden">
-        <FaBars />
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
       {/* Mobile Menu */}
-      <ul className="absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center">
-        <li>Home</li>
-        <li>About</li>
-        <li>Experience</li>
-        <li>Skills</li>
-        <li>Work</li>
-        <li>Contact</li>
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Experience</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Work</li>
+        <li className="py-6 text-4xl">Contact</li>
       </ul>
 
       {/* Social Icons */}
