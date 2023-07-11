120789573c3ddae2db30d5df08c7b63ee8e19fe5
Mohamed Amin <mohamedamin2009ster@gmail.com>
2023-07-11 13:17:52 -0700

    fixed imported icon on line 11. Imported more icons like GitHub, E-Mail, and Resume

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,7 +8,7 @@ import {
   FaFacebook,
 } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
-import { BsFillPersonalLinesFill } from "react-icons/bs";
+import { BsPersonLinesFill } from "react-icons/bs";
 import ProfilePicture from "../Assets/profilepicture.png";
 
 const Navbar = () => {
@@ -53,14 +53,45 @@ const Navbar = () => {
       </ul>
 
       {/* Social Icons */}
-      <div className="flex fixed flex-col top-[35%] left-0">
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
+          {/* LinkedIn Icon */}
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
             <a
               className="flex justify-between items-center w-full text-gray-300"
               href="/"
             >
               LinkedIn <FaLinkedin size={30} />
+            </a>
+          </li>
+
+          {/* GitHub Icon*/}
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              GitHub <FaGithub size={30} />
+            </a>
+          </li>
+
+          {/* E-Mail Icon*/}
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-green-600">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              E-Mail <HiOutlineMail size={30} />
+            </a>
+          </li>
+
+          {/* Resume Icon*/}
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              Resume <BsPersonLinesFill size={30} />
             </a>
           </li>
         </ul>
